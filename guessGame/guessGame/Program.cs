1eaac985e2965fec549d423639aa4a52e445958d
Holger <horol18@student.sdu.dk>
2019-03-08 17:23:04 +0100

    Init

---
+++ guessGame/guessGame/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guessGame
+{
+    class Program
+    {
+
+        const int MaxLives = 10;
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
